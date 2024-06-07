21313809a7c87a2c342820cf6ddb0b0a8982cfcc
HeniHes <heni.es.paypal@gmail.com>
2024-06-07 17:43:43 +0100

    updates

--- stateproject/src/App.jsx
+++ stateproject/src/App.jsx
@@ -3,18 +3,26 @@ import { Counter } from "./Counter";
 
 function App() {
   const [name, setName] = useState("Heni");
-  const [age, setAge] = useState(27);
+  // const [age, setAge] = useState(27);
 
-  function handleClick() {
-    setName("Hcini");
-    setAge((currentAge) => {
-      return currentAge + 1;
-    });
-    setAge((currentAge) => {
-      return currentAge + 1;
-    });
-  }
-  return <Counter></Counter>;
+  // function handleClick() {
+  //   setName("Hcini");
+  //   setAge((currentAge) => {
+  //     return currentAge + 1;
+  //   });
+  //   setAge((currentAge) => {
+  //     return currentAge + 1;
+  //   });
+  // }
+  console.log(name);
+
+  return (
+    <input
+      type="text"
+      value={name}
+      onChange={(ed) => setName(ed.target.value)}
+    ></input>
+  );
 }
 
 export default App;
